0fbd57f9ff39bc947126eda0918aed38f53ca5bb
maojianmiao <jianmiao.mao@outlook.com>
2016-09-25 21:01:16 +0800

    url

--- NewSite/urls.py
+++ NewSite/urls.py
@@ -2,12 +2,11 @@ from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
 from plus.views import home
-<<<<<<< HEAD
+
 import plus.views
 from django.contrib.auth.views import login, logout
 import users.views
-=======
->>>>>>> 37bc01fdd90a4318b1b9c8463761a5b5fcab5162
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -18,7 +17,6 @@ urlpatterns = patterns('',
     #url(r'^admin/', include(admin.site.urls)),
     url(r'^user/',include('users.urls')),
     url(r'home/$',home),
-<<<<<<< HEAD
     url(r'^login/$',  login),
     url(r'^logout/$', logout),
     url(r'^test/session/$',plus.views.testSession),
@@ -27,6 +25,5 @@ urlpatterns = patterns('',
     url(r'^test/',plus.views.dobject),
     url(r'^finance/',plus.views.finance),
     url(r'^tech/',plus.views.tech),
-=======
->>>>>>> 37bc01fdd90a4318b1b9c8463761a5b5fcab5162
+
 )
